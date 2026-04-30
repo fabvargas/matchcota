f2438789e33e85512ef18acd2725916e2da5564a
Santiago GZ <sgallegoz02@gmail.com>
2026-04-30 00:18:44 -0400

    Se agrega page de API, que trae informacion e imagenes de mascotas de dos APIs distintas, y las integra entre sí. Se agrega barra de busqueda

---
+++ app/inforazas/page.tsx
@@ -0,0 +1,10 @@
+import Apiraza from "@/frontend/components/ApiRaza";
+
+export default function Inforazas() {
+    return (
+        <div className="max-w-7xl mx-auto px-6 py-10">
+            <Apiraza />
+
+        </div>
+    );
+}
